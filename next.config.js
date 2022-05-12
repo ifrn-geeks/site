080dae600fd0faa15c7387f4d32e03e9138c2434
Vitor Daniel <vitor036daniel@gmail.com>
2022-05-12 09:18:10 -0300

    fix redirect

--- next.config.js
+++ next.config.js
@@ -12,14 +12,5 @@ module.exports = {
   env: {
     BUILD_ID: buildId,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
-  },
-  async redirects() {
-    return [
-      {
-        source: "/hackathon",
-        destination: "https://suap.ifrn.edu.br/eventos/inscricao_publica/3161/",
-        permanent: true
-      }
-    ]
   }
 }
